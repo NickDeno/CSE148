3a9d07651673958741a7a19c5d7a456c841128a5
NickDeno <nickdenobrega@gmail.com>
2022-05-01 02:34:35 -0400

    CSE148 Content

--- DeNobrega Final Project V2/src/app/AppDemo.java
+++ DeNobrega Final Project V2/src/app/AppDemo.java
@@ -3,6 +3,7 @@ package app;
 import view.MainView;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class AppDemo extends Application {
@@ -17,6 +18,8 @@ public class AppDemo extends Application {
 		Scene scene = new Scene(mainView.getRoot(), 1000, 700);
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("DeNobrega Final Project");
+		primaryStage.getIcons().add(new Image("BookIcon.png"));
+		primaryStage.setAlwaysOnTop(true);
 		primaryStage.show();	
 	}
 }

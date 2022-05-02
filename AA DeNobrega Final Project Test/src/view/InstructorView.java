47575f735d08fc20f1f3399d89f51fc762626009
NickDeno <nickdenobrega@gmail.com>
2022-05-02 16:06:04 -0400

    Testing for Project

---
+++ AA DeNobrega Final Project Test/src/view/InstructorView.java
@@ -0,0 +1,267 @@
+package view;
+
+import java.util.Optional;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import model.Instructor;
+import model.Name;
+import model.Person;
+import model.PersonBag;
+import util.Backup;
+
+public class InstructorView {
+	private PersonBag instructorBag;
+	private VBox instructorPane;
+	
+	private TextField firstNameField;
+	private TextField lastNameField;
+	private TextField rankField;
+	private TextField salaryField;
+	private TextField idField;
+	private TextField outputField;
+	
+	private ChoiceBox<String> choiceBox;
+	private ListView<Person> listView;
+	
+	public InstructorView(PersonBag instructorBag) {
+		this.instructorBag = instructorBag;
+		
+		Text title = new Text("Instructor View");
+		title.setFill(Paint.valueOf("#ffffff"));
+		title.setFont(Font.font("Baskerville Old Face",FontWeight.BOLD, 60));
+		
+		firstNameField = new TextField();
+		firstNameField.setPrefSize(100, 30);
+		firstNameField.setPromptText("FIRST NAME");
+		
+		lastNameField = new TextField();
+		lastNameField.setPrefSize(100, 30);
+		lastNameField.setPromptText("LAST NAME");
+		
+		rankField = new TextField();
+		rankField.setPrefSize(100, 30);
+		rankField.setPromptText("RANK");
+	
+		salaryField = new TextField();
+		salaryField.setPrefSize(100, 30);
+		salaryField.setPromptText("SALARY");
+		
+		idField = new TextField();
+		idField.setPrefSize(100, 30);
+		idField.setPromptText("ID");
+		
+		Button searchBtn = new Button("SEARCH");
+		searchBtn.setPrefSize(80, 30);
+		
+		Button removeBtn = new Button("REMOVE");
+		removeBtn.setPrefSize(80, 30);
+		
+		Button insertBtn = new Button("INSERT");
+		insertBtn.setPrefSize(80, 30);
+		
+		Button updateBtn = new Button("UPDATE");
+		updateBtn.setPrefSize(80, 30);
+		
+		outputField = new TextField();
+		outputField.setPromptText("OUTPUT");
+		outputField.setMaxSize(300, 50);
+		
+		listView = new ListView<>();
+		listView.setMaxSize(700, 200);
+		
+		
+		HBox inputBox = new HBox(20);
+		inputBox.setAlignment(Pos.CENTER);
+		inputBox.getChildren().addAll(firstNameField, lastNameField, rankField, salaryField, idField);
+		
+		HBox btnBox = new HBox(20);
+		btnBox.setAlignment(Pos.CENTER);
+		btnBox.getChildren().addAll(searchBtn, removeBtn, insertBtn, updateBtn);
+		
+		choiceBox = new ChoiceBox<>();
+		choiceBox.getItems().addAll("ID", "FIRST NAME" , "LAST NAME", "RANK", "SALARY");
+		choiceBox.setValue("Search Parameter:");
+		
+		VBox outputBox = new VBox(30);
+		outputBox.setAlignment(Pos.CENTER);
+		outputBox.getChildren().addAll(outputField, listView);
+		
+		searchBtn.setOnAction(e -> {		
+			outputField.clear();
+			listView.getItems().clear();
+			String userChoice = choiceBox.getValue();
+			Person[] predicateSearch = instructorBag.search(i -> {
+				if(i instanceof Instructor) {
+					if(userChoice.equals("ID")) {
+						return i.getId().equals(idField.getText());
+					}
+					
+					if(userChoice.equals("FIRST NAME")) {
+						return i.getName().getFirstName().equals(firstNameField.getText());
+					}
+					
+					if(userChoice.equals("LAST NAME")) {
+						return i.getName().getLastName().equals(lastNameField.getText());
+					}
+					
+					if(userChoice.equals("RANK")) {
+						return ((Instructor) i).getRank().equalsIgnoreCase(rankField.getText());
+					}
+					
+					if(userChoice.equals("SALARY")) {
+						return ((Instructor) i).getSalary() == Double.parseDouble(salaryField.getText());
+					}	
+				}
+				return false;
+			});
+			
+			if(userChoice.equals("ID") && predicateSearch.length > 0) {
+				outputField.appendText("Instructor found with id " + idField.getText() + "!");
+				setTextFields(predicateSearch[0]);
+				
+			} else if(predicateSearch.length > 0) {
+				outputField.appendText("Search results:");
+				ObservableList<Person> results = FXCollections.observableArrayList(predicateSearch);
+				listView.getItems().addAll(results);
+				listView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Person>() {
+
+					@Override
+					public void changed(ObservableValue<? extends Person> observable, Person oldPerson, Person newPerson) {
+						if(newPerson != null) {
+							setTextFields(newPerson);
+						}
+					}	
+				});
+			
+			} else {	
+				outputField.appendText("No instructor matches found.");
+				clearTextFields();
+			}
+			choiceBox.setValue("Search Parameter:");	
+		});
+		
+		removeBtn.setOnAction(e -> {
+			outputField.clear();
+			if(checkTextFieldsAreValid() == true) {
+				Alert alert = new Alert(AlertType.CONFIRMATION);
+				alert.setHeaderText(null);
+				alert.setContentText("Are you sure you want to remove instructor with id " + idField.getText() + "?");
+				Optional<ButtonType> action = alert.showAndWait();
+			
+				if(action.get() == ButtonType.OK) {
+					outputField.appendText("Removed instructor with id " + idField.getText() + "!");
+					Person[] predicateDelete = instructorBag.delete(i -> i.getId().equals(idField.getText()));		
+					listView.getItems().remove(predicateDelete[0]);
+					listView.getSelectionModel().clearSelection();
+					clearTextFields();
+					Backup.backupPersonBag(instructorBag);
+				}
+			}
+		});
+		
+		insertBtn.setOnAction(e -> {
+			outputField.clear();
+			listView.getItems().clear();
+			if(checkTextFieldsAreValid() == true && idField.getText().isEmpty()) {
+				Instructor i = new Instructor(new Name(firstNameField.getText(), lastNameField.getText()), rankField.getText(), Double.parseDouble(salaryField.getText()));
+				instructorBag.insert(i);
+				outputField.appendText("Inserted Instructor!");
+				clearTextFields();
+				Backup.backupPersonBag(instructorBag);
+				
+			} else if(!idField.getText().isEmpty()) {
+				Alert alert = new Alert(AlertType.ERROR);
+				alert.setHeaderText(null);
+				alert.setContentText("Cannot insert instructor with id chosen by user. Clear id field and try again.");
+				alert.showAndWait();
+			}
+		});
+		
+		updateBtn.setOnAction(e -> {
+			outputField.clear();	
+			Person[] instructorToUpdate = instructorBag.search(s -> s.getId().equals(idField.getText()));
+			if(checkTextFieldsAreValid() == true) {
+				instructorToUpdate[0].setName(new Name(firstNameField.getText(), lastNameField.getText()));
+				((Instructor)instructorToUpdate[0]).setRank(rankField.getText());
+				((Instructor)instructorToUpdate[0]).setSalary(Double.parseDouble(salaryField.getText()));
+				listView.getSelectionModel().clearSelection();
+				outputField.appendText("Instructor with id " + idField.getText() + " was updated!");
+				clearTextFields();
+				Backup.backupPersonBag(instructorBag);
+			}	
+		});
+		
+		instructorPane = new VBox(35);
+		instructorPane.setAlignment(Pos.CENTER);
+		instructorPane.setPadding(new Insets(30));
+		instructorPane.getChildren().addAll(title, inputBox, btnBox, choiceBox, outputBox);
+		instructorPane.setStyle("-fx-background-color: linear-gradient(from 25% 25% to 100% 100%, #129fb8, #232526 )");
+		
+	}
+	
+	public VBox getInstructorPane() {
+		return instructorPane;
+	}
+	
+	private boolean checkTextFieldsAreValid() {
+		if(firstNameField.getText().isEmpty() || lastNameField.getText().isEmpty() || rankField.getText().isEmpty() || salaryField.getText().isEmpty()) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setHeaderText(null);
+			alert.setContentText("Please recheck Text Fields and try again.");
+			alert.showAndWait();
+			return false;
+			
+		} else if(!(rankField.getText().equalsIgnoreCase("Instructor") || rankField.getText().equalsIgnoreCase("Assistant Professor") || 
+				rankField.getText().equalsIgnoreCase("Associate Professor") || rankField.getText().equalsIgnoreCase("Professor"))) {
+			
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setHeaderText(null);
+			alert.setContentText("Invalid Instructor rank. Please recheck and try again.");
+			alert.showAndWait();
+			return false;
+		}
+		return true;	
+	}	
+	
+	public void setTextFields(Person p) {
+		firstNameField.setText(p.getName().getFirstName());
+		lastNameField.setText(p.getName().getLastName());
+		rankField.setText(((Instructor)p).getRank());
+		salaryField.setText(String.valueOf(((Instructor)p).getSalary()));
+		idField.setText(p.getId());
+	}
+	
+	public void clearTextFields() {
+		firstNameField.clear();
+		lastNameField.clear();
+		rankField.clear();
+		salaryField.clear();
+		idField.clear();
+	}
+	
+	public void clearAllFields() {
+		clearTextFields();
+		choiceBox.setValue("Search Parameter");
+		outputField.clear();
+		listView.getItems().clear();
+		
+	}
+}
---
+++ AA DeNobrega Final Project Test/src/view/MainView.java
@@ -0,0 +1,245 @@
+package view;
+
+import java.io.File;
+import java.util.Optional;
+
+import javafx.application.Platform;
+import javafx.geometry.Pos;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import model.PersonBag;
+import model.TextbookBag;
+import util.Backup;
+import util.Restore;
+import util.Utilities;
+
+public class MainView {
+	private BorderPane root;
+	private PersonBag personBag;
+	private TextbookBag textbookBag;
+	
+	private StudentView studentView;
+	private InstructorView instructorView;
+	private TextbookView textbookView;
+	
+	public MainView() {
+		personBag = new File("backupFolder/Persons.dat").exists() ? Restore.restorePersonBag() : new PersonBag(2000);
+		textbookBag = new File("backupFolder/Textbooks.dat").exists() ? Restore.restoreTextbookBag() : new TextbookBag(39000);
+		root = new BorderPane();	
+		studentView = new StudentView(personBag);
+		instructorView = new InstructorView(personBag);
+		textbookView = new TextbookView(textbookBag);
+		VBox startView = makeStartingView();	
+		
+		MenuBar menuBar = new MenuBar();		
+		
+		Menu fileMenu = new Menu("File");
+		Menu importMenu = new Menu("Import (FIRST LAUNCH ONLY)");
+		MenuItem importStudentsItem = new MenuItem("Import Students");
+		MenuItem importInstructorsItem = new MenuItem("Import Instructors");
+		MenuItem importTextbooksItem = new MenuItem("Import Textbooks");
+		importMenu.getItems().addAll(importStudentsItem, importInstructorsItem, importTextbooksItem);
+		Menu backupMenu = new Menu("Backup");
+		MenuItem backupPersonsItem = new MenuItem("Backup Students/Instructors");
+		MenuItem backupTextbooksItem = new MenuItem("Backup Textbooks");
+		backupMenu.getItems().addAll(backupPersonsItem, backupTextbooksItem);
+		SeparatorMenuItem separator = new SeparatorMenuItem();
+		MenuItem exitItem = new MenuItem("Exit");		
+		fileMenu.getItems().addAll(importMenu, backupMenu, separator, exitItem);
+	
+		Menu viewMenu = new Menu("View");
+		MenuItem studentViewItem = new MenuItem("Student View");
+		MenuItem instructorViewItem = new MenuItem("Instructor View");
+		MenuItem textbookViewItem = new MenuItem("Textbook View");	
+		SeparatorMenuItem separator2 = new SeparatorMenuItem();
+		MenuItem mainMenuViewItem = new MenuItem("Return to Menu");
+		viewMenu.getItems().addAll(studentViewItem, instructorViewItem, textbookViewItem, separator2, mainMenuViewItem);
+		
+		Menu clearMenu = new Menu("Clear");
+		MenuItem clearTextFieldsItem = new MenuItem("Clear Text Fields");
+		MenuItem clearAllFieldsItem = new MenuItem("Clear All Fields");
+		clearMenu.getItems().addAll(clearTextFieldsItem, clearAllFieldsItem);
+		
+		importStudentsItem.setOnAction(e -> {
+			Alert alert = new Alert(AlertType.CONFIRMATION);
+			alert.setHeaderText(null);
+			alert.setContentText("Importing students should only be done on first launch. Do you want to continue?");
+			Optional<ButtonType> action = alert.showAndWait();
+			if(action.get() == ButtonType.OK) {
+				Utilities.importStudents(personBag);
+				Backup.backupPersonBag(personBag);
+				Alert successAlert = new Alert(AlertType.INFORMATION);
+				successAlert.setTitle("Import Successful");
+				successAlert.setHeaderText(null);
+				successAlert.setContentText("Students have been imported!");
+				successAlert.showAndWait();
+			}
+		});
+		
+		importInstructorsItem.setOnAction(e -> {
+			Alert alert = new Alert(AlertType.CONFIRMATION);
+			alert.setHeaderText(null);
+			alert.setContentText("Importing Instructors should only be done on first launch. Do you want to continue?");
+			Optional<ButtonType> action = alert.showAndWait();
+			if(action.get() == ButtonType.OK) {
+				Utilities.importInstructors(personBag);
+				Backup.backupPersonBag(personBag);
+				Alert successAlert = new Alert(AlertType.INFORMATION);
+				successAlert.setTitle("Import Successful");
+				successAlert.setHeaderText(null);
+				successAlert.setContentText("Instructors have been imported!");
+				successAlert.showAndWait();
+			}
+		});
+		
+		importTextbooksItem.setOnAction(e -> {
+			Alert alert = new Alert(AlertType.CONFIRMATION);
+			alert.setHeaderText(null);
+			alert.setContentText("Importing Textbooks should only be done on first launch. Do you want to continue?");
+			Optional<ButtonType> action = alert.showAndWait();
+			if(action.get() == ButtonType.OK) {
+				Utilities.importTextbooks(textbookBag);
+				Backup.backupTextbookBag(textbookBag);
+				Alert successAlert = new Alert(AlertType.INFORMATION);
+				successAlert.setTitle("Import Successful");
+				successAlert.setHeaderText(null);
+				successAlert.setContentText("Textbooks have been imported!");
+				successAlert.showAndWait();
+			}
+		});
+		
+		backupPersonsItem.setOnAction(e -> {
+			Backup.backupPersonBag(personBag);
+			Alert alert = new Alert(AlertType.INFORMATION);
+			alert.setTitle("Backup Successful");
+			alert.setHeaderText(null);
+			alert.setContentText("Students and Instructors have been backed up!");
+			alert.showAndWait();
+		});	
+		
+		backupTextbooksItem.setOnAction(e -> {
+			Backup.backupTextbookBag(textbookBag);
+			Alert alert = new Alert(AlertType.INFORMATION);
+			alert.setTitle("Backup Successful");
+			alert.setHeaderText(null);
+			alert.setContentText("Textbooks have been backed up!");
+			alert.showAndWait();
+		});
+		
+		exitItem.setOnAction(e -> {
+			Alert alert = new Alert(AlertType.CONFIRMATION);
+			alert.setHeaderText(null);
+			alert.setContentText("Are you sure you want to exit? Students, Instructors, and Textbooks will be backed up on exit.");
+			Optional<ButtonType> action = alert.showAndWait();
+			if(action.get() == ButtonType.OK) {
+				Backup.backupPersonBag(personBag);
+				Backup.backupTextbookBag(textbookBag);
+				Platform.exit();
+			}
+		});
+		
+		studentViewItem.setOnAction(e -> {
+			root.setCenter(studentView.getStudentPane());
+			instructorView.clearAllFields();
+			textbookView.clearAllFields();
+		});
+		
+		instructorViewItem.setOnAction(e -> {
+			root.setCenter(instructorView.getInstructorPane());
+			studentView.clearAllFields();
+			textbookView.clearAllFields();
+		});
+		
+		textbookViewItem.setOnAction(e -> {
+			root.setCenter(textbookView.getTextbookPane());
+			studentView.clearAllFields();
+			instructorView.clearAllFields();
+		});	
+		
+		mainMenuViewItem.setOnAction(e -> {
+			Alert alert = new Alert(AlertType.CONFIRMATION);
+			alert.setHeaderText(null);
+			alert.setContentText("Are you sure you want to return to main menu?");
+			Optional<ButtonType> action = alert.showAndWait();
+			if(action.get() == ButtonType.OK) {
+				root.setCenter(startView);
+				studentView.clearAllFields();
+				instructorView.clearAllFields();
+				textbookView.clearAllFields();
+			}
+		});
+
+		clearTextFieldsItem.setOnAction(e -> {
+			if(root.getCenter().equals(studentView.getStudentPane())) {
+				studentView.clearTextFields();	
+			}else if(root.getCenter().equals(instructorView.getInstructorPane())){
+				instructorView.clearTextFields();	
+			}else {
+				textbookView.clearTextFields();
+			}
+		});
+		
+		clearAllFieldsItem.setOnAction(e -> {
+			if(root.getCenter().equals(studentView.getStudentPane())) {
+				studentView.clearAllFields();	
+			}else if(root.getCenter().equals(instructorView.getInstructorPane())){
+				instructorView.clearAllFields();	
+			}else {
+				textbookView.clearAllFields();
+			}
+		});
+		
+		menuBar.getMenus().addAll(fileMenu, viewMenu, clearMenu);
+		root.setTop(menuBar);
+		root.setCenter(startView);
+	}
+
+	public BorderPane getRoot() {
+		return root;
+	}
+	
+	public VBox makeStartingView() {
+		Text title = new Text("Main Menu");
+		title.setFill(Paint.valueOf("#ffffff"));
+		title.setFont(Font.font("Baskerville Old Face",FontWeight.BOLD, 100));
+		
+		Button studentViewBtn = new Button("Student View");
+		studentViewBtn.setPrefSize(140, 50);
+		studentViewBtn.setFont(new Font("Bookman Old Style", 14));
+		studentViewBtn.setOnAction(e -> root.setCenter(studentView.getStudentPane()));
+		
+		Button instructorViewBtn = new Button("Instructor View");
+		instructorViewBtn.setPrefSize(140, 50);
+		instructorViewBtn.setFont(new Font("Bookman Old Style", 14));
+		instructorViewBtn.setOnAction(e -> root.setCenter(instructorView.getInstructorPane()));
+		
+		Button textbookViewBtn = new Button("Textbook View");
+		textbookViewBtn.setPrefSize(140, 50);
+		textbookViewBtn.setFont(new Font("Bookman Old Style", 14));
+		textbookViewBtn.setOnAction(e -> root.setCenter(textbookView.getTextbookPane()));
+		
+		HBox buttonBox = new HBox(40);
+		buttonBox.getChildren().addAll(studentViewBtn, instructorViewBtn, textbookViewBtn);
+		buttonBox.setAlignment(Pos.CENTER);
+		
+		VBox startView = new VBox(50);
+		startView.getChildren().addAll(title, buttonBox);
+		startView.setAlignment(Pos.CENTER);
+		startView.setStyle("-fx-background-color: linear-gradient(from 25% 25% to 100% 100%, #9e374a, #232526 )");
+		
+		return startView;
+	}	
+}
